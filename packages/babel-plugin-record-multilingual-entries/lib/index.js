2f83dedc3a7d55d645c6af29c5d0085240991d84
jonas.ge <jonas.ge@jonas2ge.local>
2024-04-28 11:57:59 +0800

    feat: change file

--- packages/babel-plugin-record-multilingual-entries/lib/index.js
+++ packages/babel-plugin-record-multilingual-entries/lib/index.js
@@ -2,33 +2,33 @@
 const defaultMultilingualList = ['_vm.$t', 'this.$t', 'this.$i18n.t']
 
 module.exports = ({ types }, options) => {
-	const cusCompiledMultilingualList = options.cusCompiledMultilingualList
+    const cusCompiledMultilingualList = options.cusCompiledMultilingualList;
 
-	let multilingualList = []
+    let multilingualList = [];
 
-	if(cusCompiledMultilingualList?.length === 0){
-		// throw new Error('使用了该插件，但是 传入的 cusCompiledMultilingualList 为空数组， 请检查 cusCompiledMultilingualList')
-		multilingualList = defaultMultilingualList
-	}
+    if (cusCompiledMultilingualList?.length === 0) {
+        // throw new Error('使用了该插件，但是 传入的 cusCompiledMultilingualList 为空数组， 请检查 cusCompiledMultilingualList')
+        multilingualList = defaultMultilingualList;
+    }
 
-	multilingualList = cusCompiledMultilingualList || defaultMultilingualList
+    multilingualList = cusCompiledMultilingualList || defaultMultilingualList;
 
     return {
         visitor: {
             CallExpression(astPath, state) {
-                const calleeName = astPath.get('callee').toString()
+                const calleeName = astPath.get('callee').toString();
 
-				if(defaultMultilingualList.indexOf(calleeName) !== -1){
-                    const filename = state.file.opts.filename
+                if (defaultMultilingualList.indexOf(calleeName) !== -1) {
+                    const filename = state.file.opts.filename;
 
-					const firstArgsNodeAst = astPath.get('arguments.0')
-					
-					if(types.isStringLiteral(firstArgsNodeAst)){
-                        const firstArgsNodeAstVal = firstArgsNodeAst.node.value
-                        options.resultList.push(firstArgsNodeAstVal)
-                    }else{
-                        options.resultList.push(firstArgsNodeAst.toString())
-                        options.checkFileObj[filename] = true
+                    const firstArgsNodeAst = astPath.get('arguments.0');
+
+                    if (types.isStringLiteral(firstArgsNodeAst)) {
+                        const firstArgsNodeAstVal = firstArgsNodeAst.node.value;
+                        options.resultList.push(firstArgsNodeAstVal);
+                    } else {
+                        options.resultList.push(firstArgsNodeAst.toString());
+                        options.checkFileObj[filename] = true;
                     }
                 }
             }
